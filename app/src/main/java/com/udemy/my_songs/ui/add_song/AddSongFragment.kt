2bcf0ecbb25eee05f34ef264f6e0427d19001b12
Pablo Morejón Segura <p.segura@minddistrict.com>
2019-10-13 04:21:43 +0200

    Create AddSong files

---
+++ app/src/main/java/com/udemy/my_songs/ui/add_song/AddSongFragment.kt
@@ -0,0 +1,6 @@
+package com.udemy.my_songs.ui.add_song
+
+import androidx.fragment.app.Fragment
+
+class AddSongFragment : Fragment() {
+}
---
+++ app/src/main/java/com/udemy/my_songs/ui/add_song/AddSongViewModel.kt
@@ -0,0 +1,7 @@
+package com.udemy.my_songs.ui.add_song
+
+import androidx.lifecycle.ViewModel
+
+class AddSongViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
