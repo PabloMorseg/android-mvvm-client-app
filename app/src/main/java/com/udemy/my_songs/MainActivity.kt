b6622a2bea1d6c3c3af978fee4ed59a9a16830c9
Pablo Morejón Segura <p.segura@minddistrict.com>
2019-10-13 03:27:27 +0200

    Add configureRecyclerView

--- app/src/main/java/com/udemy/my_songs/MainActivity.kt
+++ app/src/main/java/com/udemy/my_songs/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.udemy.my_songs
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.snackbar.Snackbar
+import com.udemy.my_songs.ui.SongsRecyclerViewAdapter
+import com.udemy.my_songs.utils.Mocker
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        configureRecyclerView()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun configureRecyclerView() {
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = SongsRecyclerViewAdapter(
+            Mocker.createMockSongsList(50)
+
+        )
     }
 }
