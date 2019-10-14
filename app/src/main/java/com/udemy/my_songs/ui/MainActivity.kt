39f549e7e972bafdc2a71b6139f471c3a15c7445
Pablo Morejón Segura <p.segura@minddistrict.com>
2019-10-14 09:44:47 +0200

    Refactor having a toolbar in activity

--- app/src/main/java/com/udemy/my_songs/ui/MainActivity.kt
+++ app/src/main/java/com/udemy/my_songs/ui/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.udemy.my_songs.R
 import com.udemy.my_songs.ui.main_list.SongsListFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, SongsListFragment())
