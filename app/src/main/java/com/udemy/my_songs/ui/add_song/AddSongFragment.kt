f97fc2210bc9c0eee1c0c6ccf7e0b271d6d9f882
Pablo Morejón Segura <p.segura@minddistrict.com>
2019-10-14 22:50:05 +0200

    Create AddSongFragment view

--- app/src/main/java/com/udemy/my_songs/ui/add_song/AddSongFragment.kt
+++ app/src/main/java/com/udemy/my_songs/ui/add_song/AddSongFragment.kt
@@ -1,6 +1,22 @@
 package com.udemy.my_songs.ui.add_song
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.udemy.my_songs.R
 
 class AddSongFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.add_song_fragment, container, false)
+    }
+
+    companion object {
+        operator fun invoke() = AddSongFragment()
+    }
 }
