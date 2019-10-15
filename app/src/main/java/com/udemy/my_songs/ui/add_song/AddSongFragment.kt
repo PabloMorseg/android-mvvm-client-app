248f73169d697994645cdc9f44bc614764edddf6
Pablo Morejón Segura <p.segura@minddistrict.com>
2019-10-16 00:13:29 +0200

    Get fields values and create Song with simple validation

--- app/src/main/java/com/udemy/my_songs/ui/add_song/AddSongFragment.kt
+++ app/src/main/java/com/udemy/my_songs/ui/add_song/AddSongFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.udemy.my_songs.R
+import com.udemy.my_songs.model.Song
+import kotlinx.android.synthetic.main.add_song_fragment.*
 
 class AddSongFragment : Fragment() {
 
@@ -14,6 +17,26 @@ class AddSongFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.add_song_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        saveButton.setOnClickListener {
+            saveNewSong()
+        }
+    }
+
+    private fun saveNewSong() {
+        val songName = songNameEditText.text.toString()
+        val artistName = artistNameEditText.text.toString()
+        val yearString = yearEditText.text.toString()
+        if (songName.isNotEmpty() && artistName.isNotEmpty() && yearString.isNotEmpty()) {
+            val song = Song(songName, artistName, yearString.toInt())
+            // TODO: Enviar petición para guardar la canción
+        } else {
+            Toast.makeText(context, R.string.please_fill_all_the_fields, Toast.LENGTH_SHORT)
+                .show()
+        }
     }
 
     companion object {
