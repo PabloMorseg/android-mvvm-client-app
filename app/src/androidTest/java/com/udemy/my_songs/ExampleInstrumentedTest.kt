971e9fbb044699442438c3b1a9668a82ddec59cf
Pablo Morejón Segura <p.segura@minddistrict.com>
2019-10-30 00:56:36 +0100

    Change package name to com.udemy.mysongs

--- app/src/androidTest/java/com/udemy/my_songs/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/udemy/my_songs/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.udemy.miscanciones", appContext.packageName)
+        assertEquals("com.udemy.mysongs", appContext.packageName)
     }
 }
