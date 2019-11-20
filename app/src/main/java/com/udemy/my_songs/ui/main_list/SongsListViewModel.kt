6782d61167f48a298319c8cc0c8b85ce51739da1
Pablo Morejón Segura <p.segura@minddistrict.com>
2019-11-20 02:03:04 +0100

    Catch DB error

--- app/src/main/java/com/udemy/my_songs/ui/main_list/SongsListViewModel.kt
+++ app/src/main/java/com/udemy/my_songs/ui/main_list/SongsListViewModel.kt
@@ -1,5 +1,6 @@
 package com.udemy.my_songs.ui.main_list
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.udemy.my_songs.model.Repository
@@ -8,7 +9,11 @@ import kotlinx.coroutines.Dispatchers
 class SongsListViewModel(repository: Repository) : ViewModel() {
 
     var songsLiveData = liveData(Dispatchers.IO) {
-        val songs = repository.retrieveSongs()
-        emit(songs)
+        try {
+            val songs = repository.retrieveSongs()
+            emit(songs)
+        } catch (exception: Exception) {
+            Log.e("SongsListViewModel", "Error loading songs: ${exception.message}")
+        }
     }
 }
