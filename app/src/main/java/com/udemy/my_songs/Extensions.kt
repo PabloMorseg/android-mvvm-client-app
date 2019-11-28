339ccde00667584ec88f1a549367715426957489
Pablo Morejón Segura <p.segura@minddistrict.com>
2019-11-28 02:38:11 +0100

    Add extension to hideKeyboard

--- app/src/main/java/com/udemy/my_songs/Extensions.kt
+++ app/src/main/java/com/udemy/my_songs/Extensions.kt
@@ -1,10 +1,24 @@
 package com.udemy.my_songs
 
+import android.app.Activity
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.LayoutRes
 
 fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+}
+
+fun Activity.hideKeyboard() {
+    val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager?
+    val currentFocus = currentFocus
+    if (currentFocus != null && inputManager != null) {
+        inputManager.hideSoftInputFromWindow(
+            currentFocus.windowToken,
+            InputMethodManager.HIDE_NOT_ALWAYS
+        )
+    }
 }
